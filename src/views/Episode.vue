fa741fecd07083ef5c9a333052b71ea4a9f5ef04
Edgar Dias <dias.s.edgar@gmail.com>
2024-08-14 18:47:48 +0000

    Starting to use the Store for the Episodes page (WIP)

--- src/views/Episode.vue
+++ src/views/Episode.vue
@@ -1,13 +1,22 @@
 <template lang="">
     <div>
-        <Title text="Episode Name" type="h2" />
-        <Description text="Episode Summary"/>
-        <router-link to="/shows/1">Test Show 1</router-link>
+        <template v-if="episode.id">
+            <Title :text="episode.name" type="h2" />
+            <Description :text="episode.summary"/>
+            <!--<router-link to="/shows/1">Test Show 1</router-link>-->
+            
+        </template>
+        {{store.state.episode}}
     </div>
 </template>
 <script setup>
+    import { useStore } from 'vuex';
     import Description from '../components/Description.vue';
     import Title from '../components/Title.vue';
+
+    const store = useStore();
+    const episode = computed(() => store.state.episodeStore.episode);
+
 </script>
 <style lang="">
     
