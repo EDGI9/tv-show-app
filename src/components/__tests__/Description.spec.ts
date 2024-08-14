cb4aa38e7b2da896d3bfac6f4398745e01e36947
Edgar Dias <dias.s.edgar@gmail.com>
2024-08-14 13:16:43 +0000

    Creation of Description component test

---
+++ src/components/__tests__/Description.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { mount } from '@vue/test-utils';
+import DescriptionComponent from '../Description.vue';
+
+describe('Description', () => {
+
+    let wrapper; 
+    let component;
+    const props = {
+        text: 'My Description',
+    }
+
+    beforeEach(() => {
+        wrapper = mount(DescriptionComponent, {
+            props: {
+                text: props.text,
+            },
+        });
+        component = wrapper.find('[data-testid="qa-description"]');
+    });
+
+    it('Renders the component', () => {
+        expect(component.exists()).toBeTruthy();
+    });
+
+    it('Renders the text prop correctly', () => {
+        expect(component.text()).toBe(props.text);
+    });
+
+});
