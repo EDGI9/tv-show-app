709648dbd153db3397beae99d7a0eedab0581c45
Edgar Dias <dias.s.edgar@gmail.com>
2024-08-14 13:19:44 +0000

    Creation of initial Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <component :is='type' data-testid="qa-title">{{ text }}</component>
+</template>
+
+<script setup lang="ts">
+import { TitleComponent } from "../interfaces/Components";
+
+const props = defineProps<TitleComponent>();
+
+const { text, type } = props;
+</script>
+
+<style lang="">
+    
+</style>
