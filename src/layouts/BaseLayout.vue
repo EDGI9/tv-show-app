2b514f740ba21880b78c74e12eec17bb40d88077
Edgar Dias <dias.s.edgar@gmail.com>
2024-08-14 15:37:33 +0000

    Creation of Base Layout component

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <header>
+      <ImageFrame src="/vite.svg"/>
+      <Title text="Header" type="h2" />
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <p>Footer</p>
+    </footer>
+</template>
+<script setup>
+    import Title from '../components/Title.vue';
+    import ImageFrame from '../components/ImageFrame.vue';
+</script>
+<style lang="">
+    
+</style>
