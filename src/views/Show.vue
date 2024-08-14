e18d23750d39a95a2aca7ad865a06824f36e10f9
Edgar Dias <dias.s.edgar@gmail.com>
2024-08-14 15:38:03 +0000

    Creation of Episode and Show pages

---
+++ src/views/Show.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <Title text="Show Name" type="h2" />
+        <Description text="Show Description"/>
+        <router-link to="1/episode">Test Episode 1</router-link>
+    </div>
+</template>
+<script setup>
+    import { RouterLink } from 'vue-router'
+    import Description from '../components/Description.vue';
+    import Title from '../components/Title.vue';
+</script>
+<style lang="">
+    
+</style>
