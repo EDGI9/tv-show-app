1373a9c603bc91fc26918f7f3610927152ce7fb5
Edgar Dias <dias.s.edgar@gmail.com>
2024-08-15 17:17:32 +0000

    Adding some changes to the base layout component UI (WIP)

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,12 +1,12 @@
 <template lang="">
-    <header>
-      <ImageFrame src="/vite.svg"/>
-      <Title text="Header" type="h2" />
+    <header class="c_base_layout_header">
+      <ImageFrame src="/logo.svg" class="c_base_layout_header_logo"/>
+      <Title text="Header" type="h2" class="c_base_layout_header_title"/>
     </header>
-    <main>
+    <main class="c_base_layout_main">
       <router-view />
     </main>
-    <footer>
+    <footer class="c_base_layout_footer">
       <p>Footer</p>
     </footer>
 </template>
@@ -14,6 +14,36 @@
     import Title from '../components/Title.vue';
     import ImageFrame from '../components/ImageFrame.vue';
 </script>
-<style lang="">
-    
+<style lang="css">
+  .c_base_layout_header {
+    background-color: red; /*TODO: Don't forget to replace colors*/
+    display: flex;
+    gap: 50px;
+    row-gap: 100px;
+    padding: 0 30px 0 30px;
+    align-items: center;
+  }
+
+  .c_base_layout_main {
+    flex-grow: 1;
+  }
+
+  .c_base_layout_footer {
+    background-color: red; /*TODO: Don't forget to replace colors*/
+    display: flex;
+    gap: 50px;
+    row-gap: 100px;
+    padding: 0 30px 0 30px;
+    position: sticky;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .c_base_layout_header_logo {
+    width: 40px;
+    height: 40px;
+    filter: invert(100%)
+  }
+
+
 </style>
