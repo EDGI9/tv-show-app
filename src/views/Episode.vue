e99807993008471d6463d1b11472434852c21bf5
Edgar Dias <dias.s.edgar@gmail.com>
2024-08-15 18:25:26 +0000

    Updating Episode page with new content and UI (WIP)

--- src/views/Episode.vue
+++ src/views/Episode.vue
@@ -1,22 +1,21 @@
 <template lang="">
-    <div>
-        <template v-if="episode.id">
-            <Title :text="episode.name" type="h2" />
-            <Description :text="episode.summary"/>
-            <!--<router-link to="/shows/1">Test Show 1</router-link>-->
-        </template>
+    <div class="c_episode-page">
+        <EpisodeCard :data="episode"/>
     </div>
 </template>
 <script setup>
     import { useStore } from 'vuex';
     import { computed } from 'vue';
-    import Description from '../components/Description.vue';
-    import Title from '../components/Title.vue';
+    import EpisodeCard from '../components/EpisodeCard.vue';
 
     const store = useStore();
     const episode = computed(() => store.state.episodeStore.episode);
-
 </script>
-<style lang="">
-    
+<style lang="css">
+    .c_episode-page {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
