5406c12efa988636563162371215716b673582c0
Edgar Dias <dias.s.edgar@gmail.com>
2024-08-20 12:11:56 +0000

    Creating episodeApi service test (WIP)

--- src/services/__tests__/episodeApi.spec.ts
+++ src/services/__tests__/episodeApi.spec.ts
@@ -7,5 +7,49 @@ import { transformEpisodeData } from '../../middleware/transformEpisodeData';
 import { validEpisodeDTO , transformedValidEpisodeDTO } from "../../__mocks__/middleware/episodeDto.ts";
 import { EpisodeDTO, Episode } from "../../interfaces/Data";
 
+const mockShowId :string = '1';
+const mockSeasonId : string= '2';
+const mockEpisodeId : string = '3';
+
+vi.mock('../index.ts', () => ({
+    get: vi.fn(),
+}));
+
+vi.mock('../../middleware/transformEpisodeListData.ts', () => ({
+    transformEpisodeListData: vi.fn(),
+}));
+
+vi.mock('../../middleware/transformEpisodeData.ts', () => ({
+    transformEpisodeData: vi.fn(),
+}));
+
 describe('fetchEpisodes', () => {
+    it('Should fetch episode list data and transform it', async () => {
+        const url : string = `/shows/${mockShowId}/episodes`;
+        const mockData : EpisodeDTO[] = [validEpisodeDTO];
+        const transformedData : Episode[] = [
+            transformedValidEpisodeDTO
+        ];
+        //@ts-ignore vi.Mock
+        (get as vi.Mock).mockResolvedValue(mockData);
+        //@ts-ignore vi.Mock
+        (transformEpisodeListData as vi.Mock).mockReturnValue(transformedData);
+
+        const result = await fetchEpisodes(mockShowId);
+
+        expect(get).toHaveBeenCalledWith(url);
+        expect(transformEpisodeListData).toHaveBeenCalledWith(mockData);
+        expect(result).toEqual(transformedData);
+    });
+
+    it('Should return an empty array if no data is fetched', async () => {
+        //@ts-ignore vi.Mock
+        (get as vi.Mock).mockResolvedValue(null);
+        //@ts-ignore vi.Mock
+        (transformEpisodeListData as vi.Mock).mockReturnValue([]);
+
+        const result = await fetchEpisodes(mockShowId);
+
+        expect(result).toEqual([]);
+    });
 });
